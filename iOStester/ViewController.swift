0260a49af84eb4da82e573a71758081b304e2baf
Anthony Pichardo <anthony.pichardo.453@my.csun.edu>
2016-01-16 11:34:29 -0800

    Initial Commit

---
+++ iOStester/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOStester
+//
+//  Created by Anthony on 1/16/16.
+//  Copyright (c) 2016 Anthony. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
